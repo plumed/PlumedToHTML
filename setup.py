1b0a3547455e00d342dda9195408e7e777053ff7
Gareth Tribello <gareth.tribello@gmail.com>
2022-07-18 12:57:53 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='PlumedToHTML',  
-     version='0.13',
+     version='0.14',
      author="Gareth Tribello",
      author_email="gareth.tribello@gmail.com",
      description="A package for creating pretified HTML for PLUMED files",
