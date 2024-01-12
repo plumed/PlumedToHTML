e4fce405cd8da9f0d149bc45099983efff9107b2
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-01-12 17:04:55 +0000

    Updated release number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='PlumedToHTML',  
-     version='0.51',
+     version='0.52',
      author="Gareth Tribello",
      author_email="gareth.tribello@gmail.com",
      description="A package for creating pretified HTML for PLUMED files",
