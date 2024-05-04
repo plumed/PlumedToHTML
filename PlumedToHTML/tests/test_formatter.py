2d8668a0bd80a07eef0d6ec039b7236f39b28e22
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-04 18:32:40 +0100

    Added tests on switching cells for formatter tests

--- PlumedToHTML/tests/test_formatter.py
+++ PlumedToHTML/tests/test_formatter.py
@@ -31,9 +31,16 @@ class TestPlumedFormatter(TestCase):
                tokensource = list(PlumedLexer().get_tokens(item["input"]))
                output = StringIO() 
                f.format( tokensource, output )
+               # Check for clickable labels
                self.assertTrue( compare_to_reference( output.getvalue(), item ) )
                soup = BeautifulSoup( output.getvalue(), "html.parser" )
                for val in soup.find_all("b") :
                    if "onclick" in val.attrs.keys() : 
                       vallabel = val.attrs["onclick"].split("\"")[3]
                       self.assertTrue( soup.find("span", {"id": vallabel}) )
+               # Check for switching cells
+               for val in soup.find_all(attrs={'class': 'toggler'}) :
+                   if "onclick" in val.attrs.keys() :
+                      switchval = val.attrs["onclick"].split("\"")[1]
+                      if not soup.find("span",{"id": switchval + "_long"} ) : raise Exception("Generated html is invalid as could not find " + switchval + "_long")
+                      if not soup.find("span",{"id": switchval + "_short"} ) : raise Exception("Generated html is invalid as could not find " + switchval + "_short")
