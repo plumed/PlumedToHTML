35c6f06a3df67ca3c5e2068015e398276b6a760f
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2025-06-01 19:12:19 +0100

    Added code that ensures that stderrs are always output in scrollable pre's so you can see the full log

--- src/PlumedToHTML/PlumedToHTML.py
+++ src/PlumedToHTML/PlumedToHTML.py
@@ -139,7 +139,7 @@ def test_plumed( executible, filename, header=[], printjson=False, jsondir="./",
         if len(header)>0 : print(header,file=stderr)
         print("Stderr for source: ",re.sub("^data/","",filename),"  ",file=stderr)
         print("Download: [zipped raw stdout](" + plumed_file + "." + executible + ".stdout.txt.zip) - [zipped raw stderr](" + plumed_file + "." + executible + ".stderr.txt.zip) ",file=stderr)
-        if ghmarkdown : print("{% raw %}\n<pre>",file=stderr)
+        if ghmarkdown : print("{% raw %}\n<pre style=\"overflow:scroll;\">",file=stderr)
         else : print("<pre>",file=stderr)
         # now we print the first 1000 lines of errtxtfile to errfile
         with open(errtxtfile, "r") as stdtxterr:
