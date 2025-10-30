c37856c74f593e050cf7c9c15a7597172830c353
Daniele Rapetti <iximiel@gmail.com>
2025-10-30 09:29:53 +0100

    enforcing linting and formatting

--- src/PlumedToHTML/__init__.py
+++ src/PlumedToHTML/__init__.py
@@ -1,1 +1,31 @@
-from .PlumedToHTML import test_plumed, test_and_get_html, get_html, get_html_header, compare_to_reference, get_mermaid, processMarkdown, processMarkdownString, get_javascript, get_css, getPlumedSyntax, get_cltoolarg_html, get_cltoolfile_html
+from .PlumedToHTML import (
+  test_plumed,
+  test_and_get_html,
+  get_html,
+  get_html_header,
+  compare_to_reference,
+  get_mermaid,
+  processMarkdown,
+  processMarkdownString,
+  get_javascript,
+  get_css,
+  getPlumedSyntax,
+  get_cltoolarg_html,
+  get_cltoolfile_html,
+)
+
+__all__ = [
+  "test_plumed",
+  "test_and_get_html",
+  "get_html",
+  "get_html_header",
+  "compare_to_reference",
+  "get_mermaid",
+  "processMarkdown",
+  "processMarkdownString",
+  "get_javascript",
+  "get_css",
+  "getPlumedSyntax",
+  "get_cltoolarg_html",
+  "get_cltoolfile_html",
+]
